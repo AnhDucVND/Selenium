b1ecadedf497d7bec2713eb9b3c9eec0e014c3d3
AnhDucVND <anhductung98@gmail.com>
2023-10-19 23:35:54 +0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,57 @@
-import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
-    public static void main(String[] args) {
-            WebDriver driver=null;
-            System.setProperty("webdriver.chrome.driver","D:\\AKA\\Dang ki package\\Browser\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-            driver =new ChromeDriver();
+    public static void main(String[] args) throws Exception {
+            //WebDriver driver=null;
+            System.setProperty("webdriver.chrome.driver","D:\\AKA\\Dang ki package\\Browser\\chromedriver-win64\\chromedriver.exe");
+            WebDriver driver =new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             driver.get("https://www.thegioididong.com/");
+            //Chuyển sang full màn hình
             driver.manage().window().maximize();
+
             System.out.println(driver.getTitle());
-            driver.quit();
+
+            driver.navigate().to("https://myaccount.vndirect.com.vn/");
+            Thread.sleep(2000);
+
+            String title = driver.getTitle();
+
+            System.out.println(title);
+
+            WebElement input_username= driver.findElement(By.xpath("//input[@placeholder=\"Nhập tên đăng nhập\"]"));
+            input_username.sendKeys("annhduc98");
+            //input_username.clear();
+
+            WebElement input_password= driver.findElement(By.id("typepassword"));
+            input_password.sendKeys("Ab05131998");
+            //input_username.clear();
+
+            //Click vào button Login
+            WebElement btn_Login= driver.findElement(By.xpath("//span[contains(text(),\"Đăng nhập\")]/parent::button"));
+            btn_Login.click();
+            Thread.sleep(2000);
+            boolean checkstatus =driver.findElement(By.xpath("//div[@id=\"logo-image\"]")).isDisplayed();
+
+            if(checkstatus==true){
+                System.out.println("Đã login thành công");
+            }
+
+
+
+
+            Thread.sleep(2000);
+            //Đóng trình duyệt
+            //driver.quit();
+
+
 
         }
 
---
+++ src/Student.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Student {
+
+
+     public static void main(String[] args) {
+         Scanner Student=new Scanner(System.in);
+         System.out.println("Nhập hộ và tên: ");
+         String name= Student.nextLine();
+         System.out.println("Nhập vào tuổi: ");
+         int age= Student.nextInt();
+         System.out.println("___________");
+         System.out.println("Họ và tên là: "+ name);
+         System.out.println("Tuổi là: "+ age);
+        
+
+
+
+
+
+
+
+    }
+}
